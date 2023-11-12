70c5557c0c7ad3d0698cb2d8bd50cc74fb0ecb74
ganeshkandhan17 <ganeshkandhan@gmail.com>
2023-11-12 14:36:06 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+
+var apikey="e7e6c64958424d1da1f63308231211"
+var url="http://api.weatherapi.com/v1/current.json?"
+// var q="ooty"
+async function checkweather(){
+    var q=document.getElementById("inputcity").value
+    const response= await fetch(`${url}key=${apikey}&q=${q}`);
+    if(response.status==400){
+        alert("Enter valid City Name");
+    }
+    else{
+    const data = await response.json();
+    console.log(data);
+    var mainweasthericon=document.querySelector(".mainweathericon");
+    mainweasthericon.src=data.current.condition.icon
+    var temp=document.getElementById("temp");
+    temp.innerHTML=data.current.temp_c+" °C"
+    var tempcmt=document.getElementById("temcmt")
+    tempcmt.innerText=data.current.condition.text;
+    document.getElementById("humidityval").innerText=data.current.humidity+" %"
+    document.getElementById("windval").innerText=data.current.wind_kph+" Km/hr"
+    document.getElementById("weather").style.display="block";
+    }
+    
+}
+// checkweather();
